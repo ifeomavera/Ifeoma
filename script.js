f5c91e6507d55ed91a679b83294f59988289f0ed
Ezeka ifeoma vera <ifeomaezeka01@gmail.com>
2024-08-18 15:15:21 +0100

    Add files to upload
    Addition of all the files needed

---
+++ script.js
@@ -0,0 +1,7 @@
+function togglemenu(){
+    const menu = document.querySelector(".menulinks");
+    const icon = document.querySelector(".hamburgericon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+}
